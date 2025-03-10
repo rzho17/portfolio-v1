1cf38341b96818135cd6f8718e0c51de58960794
Richard <richardzehungho@gmail.com>
2025-03-10 13:20:13 -0700

    add scroll effects for work section

--- src/components/ContactInfo.jsx
+++ src/components/ContactInfo.jsx
@@ -3,20 +3,37 @@ import styles from "./ContactInfo.module.css";
 import { IoLogoGithub } from "react-icons/io5";
 import { FaLinkedin } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa";
+import { BsFileEarmarkPersonFill } from "react-icons/bs";
 
 export default function ContactInfo() {
   return (
     <div className={styles.infoLinks}>
-      <a href="https://github.com/rzho17">
-        <IoLogoGithub className={styles.infoIcon} />
-      </a>
-      <a href="https://www.linkedin.com/in/richard-h-089b6a230/">
-        <FaLinkedin className={styles.infoIcon} />
-      </a>
-      <a href="">
-        <FaInstagram className={styles.infoIcon} />
-      </a>
-      <a href="">resume</a>
+      <ul>
+        <li>
+          <a href="https://github.com/rzho17">
+            <IoLogoGithub className={styles.infoIcon} />
+          </a>
+          Github
+        </li>
+        <li>
+          <a href="https://www.linkedin.com/in/richard-h-089b6a230/">
+            <FaLinkedin className={styles.infoIcon} />
+          </a>
+          LinkedIn
+        </li>
+        <li>
+          <a href="">
+            <FaInstagram className={styles.infoIcon} />
+          </a>
+          Socials
+        </li>
+        <li>
+          <a href="">
+            <BsFileEarmarkPersonFill className={styles.infoIcon} />
+          </a>
+          Resume
+        </li>
+      </ul>
     </div>
   );
 }
